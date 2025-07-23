d784c93fa0ea3dc7656fdc6fbfb6540ce0174a2c
Amitya31 <amitya1576@gmail.com>
2025-07-23 23:04:59 +0530

    dialog box

--- frontend/src/Components/ui/Dialog.tsx
+++ frontend/src/Components/ui/Dialog.tsx
@@ -4,7 +4,9 @@ import * as DialogPrimitive from "@radix-ui/react-dialog";
 function Dialog() {
   return (
     <div>
-       <DialogPrimitive.DialogTrigger/>
+       <DialogPrimitive.DialogTrigger>
+        
+       </DialogPrimitive.DialogTrigger>
     </div>
   )
 }
